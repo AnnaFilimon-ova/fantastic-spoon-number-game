116278853575f1273f4e4e935d4290578c43002e
AnnaFilimon-ova <annafilimon.ova6666@gmail.com>
2026-06-16 23:13:06 +0300

    match numbers v1

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import random
 
-numbers_list = [random.randint(0, 9) for _ in range(5)]
+numbers_list = random.sample(range(0, 10), 5)
 print(numbers_list)
+
 code_list = ['*', '*', '*', '*', '*']
 print(code_list)
 
-your_number = int(input("Enter a number: "))
-
+print("Enter a numbers: ")
+match_count_places = 0
+match_count = 0
 for i in numbers_list:
+    your_number = int(input())
     if your_number == i:
-        print("Match")
-    else:
-        print("No match")
+        match_count_places += 1
+    if your_number in numbers_list:
+        match_count += 1
+
+print(f"Match numbers: {match_count}")
+print(f"Match numbers in their places: {match_count_places}")
+
+#TODO: pygame
