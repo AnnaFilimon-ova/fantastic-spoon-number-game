7d2a74d1f60e12075387ef6e68d058ebdb415c43
AnnaFilimon-ova <annafilimon.ova6666@gmail.com>
2026-06-17 10:52:00 +0300

    full console game

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import random
 
 numbers_list = random.sample(range(0, 10), 5)
-print(numbers_list)
 
-code_list = ['*', '*', '*', '*', '*']
-print(code_list)
+print("* * * * *")
 
 print("Enter a numbers: ")
 match_count_places = 0
 match_count = 0
-for i in numbers_list:
-    your_number = int(input())
-    if your_number == i:
-        match_count_places += 1
-    if your_number in numbers_list:
-        match_count += 1
 
-print(f"Match numbers: {match_count}")
-print(f"Match numbers in their places: {match_count_places}")
+while match_count_places != 5:
+
+    user_numbers = input()
+    your_numbers = [int(num) for num in user_numbers.split()]
+    match_count_places = 0
+    match_count = 0
+    for i in range(5):
+        if your_numbers[i] == numbers_list[i]:
+            match_count_places += 1
+    match_count = len(set(your_numbers) & set(numbers_list))
+    print(f"Match numbers: {match_count}")
+    print(f"Match numbers in their places: {match_count_places}")
+    
+print("You win!")
 
 #TODO: pygame
